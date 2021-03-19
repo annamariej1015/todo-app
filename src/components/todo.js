053c1d5a5d320e861719cd421bc24c20178d0b58
Anna <annamariej1015@gmail.com>
2021-03-19 09:34:17 -0700

    first commit

---
+++ src/components/todo.js
@@ -0,0 +1,22 @@
+
+
+
+const TodoComponent = () =>{
+    
+        return (
+            <div>
+                <form>
+                    <div className="form-group">
+                        <label for="textarea"></label>
+                        <textarea   className="form-control" id="ToDo" 
+                        rows="1"></textarea>
+                        <button class>Add</button>
+                    </div>
+                </form>
+            </div>
+        )
+    };
+
+
+
+export default TodoComponent;
